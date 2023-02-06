c9698933d76a9acd38c65fe5d0c88c4acea46879
ling <2448364378@qq.com>
2023-02-06 09:45:35 +0800

    整理脚手架

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    host: '127.0.0.1',
+    port: 8080
+  }
 })
