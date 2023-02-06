4128d4b1cca4d6e6b84ab482a9e01ab9f1e0661b
ling <2448364378@qq.com>
2023-02-06 16:21:59 +0800

    Home组件页面搭建

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import Header from '@/components/Header/Header.vue';
 import Footer from '@/components/Footer/Footer.vue';
+import TypeNav from '@/components/TypeNav/TypeNav.vue';
 
 Vue.config.productionTip = false;
 
 //注册全局组件
 Vue.component(Header.name, Header);
 Vue.component(Footer.name, Footer);
+Vue.component(TypeNav.name, TypeNav);
 
 // 引入路由
 import router from './router';
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-    <h1>我是Home内容</h1>
+    <type-nav></type-nav>
+    <list></list>
+    <recommend></recommend>
+    <rank></rank>
+    <like></like>
+    <floor></floor>
+    <floor></floor>
+    <brand></brand>
   </div>
 </template>
 
 <script>
+import Brand from './Brand/Brand.vue';
+import Floor from './Floor/Floor.vue';
+import Like from "./Like/Like.vue";
+import List from "./List/List.vue";
+import Rank from "./Rank/Rank.vue";
+import Recommend from "./Recommend/Recommend.vue";
 export default {
-  name: 'Home'
-}
+  components: { List, Recommend, Rank, Like, Floor, Brand },
+  name: "Home",
+};
 </script>
 
 <style scoped>
-
 </style>
