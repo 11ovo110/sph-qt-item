7c31037ec335e79cf1a38d868dfa8e2fc4e163c4
ling <2448364378@qq.com>
2023-02-06 16:59:47 +0800

    安装axios, 测试接口

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,13 @@ module.exports = defineConfig({
   devServer: {
     open: true,
     host: '127.0.0.1',
-    port: 8080
+    port: 8080,
+    proxy: {
+      "/api": {
+        "target": 'http://sph-h5-api.atguigu.cn'
+        // 后端接口携带 /api 则不需要重写
+      }
+    }
   },
   lintOnSave: false
 })
